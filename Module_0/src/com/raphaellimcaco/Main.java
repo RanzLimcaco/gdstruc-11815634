8b0a27d87a594b464f299c6cb28f306769f47516
Ranz <raphaelantoniovincenzo.limcaco@benilde.edu.ph>
2021-08-30 17:52:53 +0800

    trying out the module 0

--- Module_0/src/com/raphaellimcaco/Main.java
+++ Module_0/src/com/raphaellimcaco/Main.java
@@ -3,9 +3,26 @@ package com.raphaellimcaco;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
-        // try new comment
 
+        int[] numbers = new int[5];
+
+        numbers[0] = 70;
+        numbers[1] = 80;
+        numbers[2] = 75;
+        numbers[3] = 650;
+        numbers[4] = 30;
+
+        int index = -1;
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (numbers[i] == 75)
+            {
+                index = i;
+                break;
+            }
+
+        }
+        System.out.println(index);
     }
 }
